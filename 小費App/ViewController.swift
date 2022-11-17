4ecdbe273c1f5a513914d3c4ce69cf806be7f27f
Shelley Hsieh <shuan0720@gmail.com>
2022-11-17 16:37:59 +0800

    Initial Commit

---
+++ 小費App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  小費App
+//
+//  Created by shelley on 2022/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
